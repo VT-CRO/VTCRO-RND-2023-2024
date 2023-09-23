0b50a40a485c55e12f2f5ab5252520643e4e3825
jdv52 <jaysondv0928@gmail.com>
2023-09-23 15:52:18 -0400

    Created motor control folder structure

--- src/main.cpp
+++ src/main.cpp
@@ -3,46 +3,48 @@
 
 static void task1(void*) {
   while (true) {
-    ::digitalWriteFast(arduino::LED_BUILTIN, arduino::LOW);
-    ::vTaskDelay(pdMS_TO_TICKS(500));
+    digitalWriteFast(arduino::LED_BUILTIN, arduino::LOW);
+    vTaskDelay(pdMS_TO_TICKS(500));
 
-    ::digitalWriteFast(arduino::LED_BUILTIN, arduino::HIGH);
-    ::vTaskDelay(pdMS_TO_TICKS(500));
+    digitalWriteFast(arduino::LED_BUILTIN, arduino::HIGH);
+    vTaskDelay(pdMS_TO_TICKS(500));
   }
 }
 
 static void task2(void*) {
   while (true) {
-    ::Serial.println("TICK");
-    ::vTaskDelay(pdMS_TO_TICKS(1'000));
+    Serial.println("TICK");
+    vTaskDelay(pdMS_TO_TICKS(1'000));
 
-    ::Serial.println("TOCK");
-    ::vTaskDelay(pdMS_TO_TICKS(1'000));
+    Serial.println("TOCK");
+    vTaskDelay(pdMS_TO_TICKS(1'000));
   }
 }
 
 FLASHMEM __attribute__((noinline)) void setup() {
-  ::Serial.begin(115'200);
-  ::pinMode(arduino::LED_BUILTIN, arduino::OUTPUT);
-  ::digitalWriteFast(arduino::LED_BUILTIN, arduino::HIGH);
+  Serial.begin(115'200);
+  pinMode(arduino::LED_BUILTIN, arduino::OUTPUT);
+  digitalWriteFast(arduino::LED_BUILTIN, arduino::HIGH);
 
-  ::delay(5'000);
+  delay(5'000);
 
   if (CrashReport)  {
-    ::Serial.print(CrashReport);
-    ::Serial.println();
-    ::Serial.flush();
+    Serial.print(CrashReport);
+    Serial.println();
+    Serial.flush();
   }
 
-  ::Serial.println(PSTR("\r\nBooting FreeRTOS kernel " tskKERNEL_VERSION_NUMBER ". Built by gcc " __VERSION__ " (newlib " _NEWLIB_VERSION ") on " __DATE__ ". ***\r\n"));
+  Serial.println(PSTR("\r\nBooting FreeRTOS kernel " tskKERNEL_VERSION_NUMBER ". Built by gcc " __VERSION__ " (newlib " _NEWLIB_VERSION ") on " __DATE__ ". ***\r\n"));
 
-  ::xTaskCreate(task1, "task1", 128, nullptr, 2, nullptr);
-  ::xTaskCreate(task2, "task2", 128, nullptr, 2, nullptr);
+  // create tasks
+  xTaskCreate(task1, "task1", 128, nullptr, 2, nullptr);
+  xTaskCreate(task2, "task2", 128, nullptr, 2, nullptr);
 
-  ::Serial.println("setup(): starting scheduler...");
-  ::Serial.flush();
+  // start the scheduler
+  Serial.println("setup(): starting scheduler...");
+  Serial.flush();
 
-  ::vTaskStartScheduler();
+  vTaskStartScheduler();
 }
 
 void loop() {
