5e69920043e81a9e290f5c5f16d2055017dcb968
Devyn04 <125822672+Devyn04@users.noreply.github.com>
2023-12-01 12:03:34 -0500

    Update QTR_8.h to include a value for the array length

--- src/QTR_8.h
+++ src/QTR_8.h
@@ -5,9 +5,10 @@ typedef struct
   uint16_t* pins;
   uint16_t* data;
   uint16_t sampleSize;
+  uint16_t arrayLength;
 } QTR_t;
 
-QTR_t QTR_init(uint16_t pins[], uint16_t data[], uint16_t sampleSize);
+QTR_t QTR_init(uint16_t pins[], uint16_t data[], uint16_t sampleSize, uint16_t arrayLength);
 
 int changeBit (int initialVal);
 
