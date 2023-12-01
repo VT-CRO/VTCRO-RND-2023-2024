26a23c40a885a755014005b931ef20f7145abdfd
Devyn04 <125822672+Devyn04@users.noreply.github.com>
2023-12-01 11:50:22 -0500

    Update main.cpp to be able to use more arrays

--- src/main.cpp
+++ src/main.cpp
@@ -68,22 +68,30 @@ uint16_t s4 = A4;
 uint16_t s5 = A5;
 uint16_t s6 = A6;
 uint16_t s7 = A7;
-uint16_t sensorPins[] = {s0, s1, s2, s3, s4, s5, s6, s7};
-uint16_t readings[8];
+uint16_t s8 = A8;
+uint16_t s9 = A9;
+uint16_t s10 = A10;
+uint16_t s11 = A11;
+uint16_t s12 = A12;
+uint16_t s13 = A13;
+uint16_t s14 = A14;
+uint16_t s15 = A15;
+uint16_t sensorPins[] = {s0, s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13, s14, s15};
+uint16_t readings[16];
 QTR_t data{};
 
-void setup() {
+FLASHMEM __attribute__((noinline)) void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
-  data = QTR_init(sensorPins, readings, 4);
+  Serial.begin(115'200);
+  data = QTR_init(sensorPins, readings, 4, 16);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   
-  sensorRead(data.data, 8);
+  sensorRead(data.arrayLength);
 
-  for (uint8_t i = 0; i < 8; i++)
+  for (uint8_t i = 0; i < data.arrayLength; i++)
   {
     Serial.print("Value at ");
     Serial.print(i);
@@ -95,7 +103,7 @@ void loop() {
   delay(1000);
 }
 
-void sensorRead(uint16_t* sensorValues, int sensorCount)
+void sensorRead(int sensorCount)
 {
     for (uint8_t r = 0; r < sensorCount; r += 1)
       {
@@ -111,16 +119,16 @@ void sensorRead(uint16_t* sensorValues, int sensorCount)
         }
       }
 
-  for (uint8_t k = 0; k < 8; k += 1)
+  for (uint8_t k = 0; k < sensorCount; k += 1)
       {
         data.data[k] = (data.data[k] + (4 >> 1)) /
           4;
       }
 }
 
-QTR_t QTR_init(uint16_t pins[], uint16_t data[], uint16_t sampleSize)
+QTR_t QTR_init(uint16_t pins[], uint16_t data[], uint16_t sampleSize, uint16_t arrayLength)
 {
-   QTR_t values = {pins, data, sampleSize};
+   QTR_t values = {pins, data, sampleSize, arrayLength};
    return values;
 }
 
