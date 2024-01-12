62bd7ce744bbb319896686ad31f5e195890aa801
jdv52 <jaysondv0928@gmail.com>
2024-01-11 22:32:11 -0500

    encoder pin initialization

--- lib/Encoder/src/Encoder.cpp
+++ lib/Encoder/src/Encoder.cpp
@@ -1,31 +1,94 @@
 #include "Encoder.h"
-#include "arduino_freertos.h"
 
-void Encoder::init()
+const Encoder::ENC_Channel_t Encoder::channel[] = {	
+	{0,&IMXRT_ENC1, IRQ_ENC1, isrEnc1, 66, 67, 68, 69, 70,&CCM_CCGR4,CCM_CCGR4_ENC1(CCM_CCGR_ON)},  //this is a dummy entry - use 1-4 for channels
+	{1, &IMXRT_ENC1, IRQ_ENC1, isrEnc1, 66, 67, 68, 69, 70,&CCM_CCGR4,CCM_CCGR4_ENC1(CCM_CCGR_ON)},
+	{2, &IMXRT_ENC2, IRQ_ENC2, isrEnc2, 71, 72, 73, 74, 75,&CCM_CCGR4,CCM_CCGR4_ENC2(CCM_CCGR_ON)},
+	{3, &IMXRT_ENC3, IRQ_ENC3, isrEnc3, 76, 77, 78, 79, 80,&CCM_CCGR4,CCM_CCGR4_ENC3(CCM_CCGR_ON)},
+	{4, &IMXRT_ENC4, IRQ_ENC4, isrEnc4, 81, 82, 83, 84, 85,&CCM_CCGR4,CCM_CCGR4_ENC4(CCM_CCGR_ON)}
+};
+const uint8_t Encoder::_channel_count =  (sizeof(Encoder::channel)/sizeof(Encoder::channel[0]));
+
+Encoder::Encoder(uint8_t channel, uint8_t phaseA, uint8_t phaseB, uint8_t index, uint8_t home, uint8_t trigger, uint8_t pin_pus)
 {
-    // enable bus clock
-    // map digital pin to encoder pin
+    // check if encoder channel is valid
+    if (channel < _channel_count) {
+        // enable xbar clock
+        CCM_CCGR2 |= CCM_CCGR2_XBAR1(CCM_CCGR_ON);
 
-    // enable ccm
+        // map digital pin to encoder pin
+        xbara_pin_map(phaseA,  PHASEA,  pin_pus);
+        xbara_pin_map(phaseB,  PHASEB,  pin_pus);
+        xbara_pin_map(index,   INDEX,   pin_pus);
+        xbara_pin_map(home,    HOME,    pin_pus);
+        xbara_pin_map(trigger, TRIGGER, pin_pus);
+    }
 }
 
-void xbara_pin_map(uint16_t pin, uint16_t func, uint8_t pus)
-{
-    digital_pin_bitband_and_config_table_struct *p;
 
-    p = digital_pin_to_info_PGM + pin;
+void Encoder::xbara_pin_map(uint16_t pin, Encoder_pin func, uint8_t pus)
+{
+    int pin_idx = 0;
+
+    for (; pin_idx < count_pin_to_xbar_info; ++pin_idx)
+    {
+        if (pin_to_xbar_info[pin_idx].pin == pin)
+
+            break;
+    }
+
+    IOMUXC_XBAR1_IN02_SELECT_INPUT;
+
+    // The pin is not a valid xbar pin
+    if (pin_idx == count_pin_to_xbar_info) return;
+
+    *(portConfigRegister(pin)) = pin_to_xbar_info[pin_idx].mux_val;
+
+    // TODO: set PUS configuration
+
+    // MUX xbar
+    if (pin_to_xbar_info[pin_idx].select_input_register)
+        *(pin_to_xbar_info[pin_idx].select_input_register) = pin_to_xbar_info[pin_idx].select_val;
+
+    switch(func)
+    {
+        case PHASEA:
+            xbar_connect(pin_to_xbar_info[pin_idx].xbar_in_index, channel[_encoder_channel].phaseA);
+            break;
+        case PHASEB:
+            xbar_connect(pin_to_xbar_info[pin_idx].xbar_in_index, channel[_encoder_channel].phaseB);
+            break;
+        case HOME:
+            xbar_connect(pin_to_xbar_info[pin_idx].xbar_in_index, channel[_encoder_channel].home);
+            break;
+        case INDEX:
+            xbar_connect(pin_to_xbar_info[pin_idx].xbar_in_index, channel[_encoder_channel].index);
+            break;
+        case TRIGGER:
+            xbar_connect(pin_to_xbar_info[pin_idx].xbar_in_index, channel[_encoder_channel].trigger);
+            break;
+        default:
+            break;
+    }
 }
 
 // map input to output
-void xbar_connect(uint16_t in, uint16_t out)
+void Encoder::xbar_connect(unsigned int input, unsigned int output)
 {
-    if (in >= 88) return;
-    if (out >= 132) return;
-    volatile uint16_t *xbar = &XBARA1_SEL0 + (out / 2);
-    if (out % 2)
-    {
-        *xbar = (*xbar & 0xFF00) | (in << 8);
-    }
-    else
-        *xbar = (*xbar & 0x00FF) | in;
+	if (input >= 88) return;
+	if (output >= 132) return;
+#if 1
+	volatile uint16_t *xbar = &XBARA1_SEL0 + (output / 2);
+	uint16_t val = *xbar;
+	if (!(output & 1)) {
+		val = (val & 0xFF00) | input;
+	} else {
+		val = (val & 0x00FF) | (input << 8);
+	}
+	*xbar = val;
+#else
+	// does not work, seems 8 bit access is not allowed
+	volatile uint8_t *xbar = (volatile uint8_t *)XBARA1_SEL0;
+	xbar[output] = input;
+#endif
 }
--- lib/Encoder/src/Encoder.h
+++ lib/Encoder/src/Encoder.h
@@ -1,26 +1,70 @@
 #ifndef ENCODER_H
 #define ENCODER_H
 
+#include "arduino_freertos.h"
+
 #include "Subject.hpp"
+
+enum Encoder_pin {
+  PHASEA, PHASEB, HOME, INDEX, TRIGGER
+};
+typedef enum Encoder_pin Encoder_pin;
 
 class Encoder : public Subject<int> {
 public:
-  Encoder();
+  Encoder(uint8_t channel, uint8_t phaseA, uint8_t phaseB, uint8_t index, uint8_t home, uint8_t trigger, uint8_t pin_pus);
   Encoder(Encoder &&) = default;
   Encoder(const Encoder &) = default;
   Encoder &operator=(Encoder &&) = default;
   Encoder &operator=(const Encoder &) = default;
   ~Encoder();
 
+  typedef struct ENC_Props {
+    int hi;
+  } ENC_Props;
+
+  void enableInterrupts();
+  void disableInterrupts();
+  void clearStatusFlag();
+
+  void setConfig(ENC_Props);
   void init();
+  void write(uint32_t val);
+  void getRevolutions();
+  uint16_t getPosition();
+  uint16_t getPositionDifference();
 
 private:
-  struct Encoder_props {};
+  typedef struct {
+		uint8_t			enc_ch;		
+		volatile IMXRT_ENC_t* 	ENC;
+		IRQ_NUMBER_t	interrupt;
+		void     		(*isr)();
+		uint16_t		phaseA;
+		uint16_t		phaseB;
+		uint16_t		index;
+		uint16_t		home;
+		uint16_t		trigger;
+		volatile uint32_t *clock_gate_register;
+		uint32_t 		clock_gate_mask;
+	} ENC_Channel_t;
 
-  typedef struct Encoder_props Encoder_props;
+  static const uint8_t _channel_count;
+  static uint8_t _encoder_channel;
+  static const ENC_Channel_t channel[];
 
-  Encoder_props props;
-  int count;
+  static Encoder *instances[5];
+
+  static void isrEnc1();
+	static void isrEnc2();
+	static void isrEnc3();
+	static void isrEnc4();
+  void isr();
+
+  void xbara_pin_map(uint16_t pin, Encoder_pin func, uint8_t PUS);
+  void xbar_connect(unsigned int input, unsigned int output);
+
+  // Counts are stored by the super class
 };
 
 #endif // !ENCODER_H
