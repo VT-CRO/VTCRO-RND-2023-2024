ebe9aadb6c58495a4d7f085c1ba6f1780aa9010c
dmarcelli <spartanxd31@vt.edu>
2024-02-12 17:32:41 -0500

    Added all hardware related macros in HardWare Def

--- src/HardwareDefs.h
+++ src/HardwareDefs.h
@@ -9,3 +9,25 @@
 
 #define MOTOR_BR_IN1 9
 #define MOTOR_BR_IN2 8
+
+#define QTR_0 14
+#define QTR_1 15
+#define QTR_2 16
+#define QTR_3 17
+#define QTR_4 18
+#define QTR_5 19
+#define QTR_6 20
+#define QTR_7 21
+#define QTR_8 22
+#define QTR_9 23
+#define QTR_10 24
+#define QTR_11 25
+#define QTR_12 26
+#define QTR_13 39
+#define QTR_14 40
+#define QTR_15 41
+
+#define LENGTH 11 // inches
+#define WIDTH 11  // inches
+
+#define WHEEL_RADIUS 1 // inches
