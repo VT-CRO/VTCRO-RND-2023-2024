b51c74649a91cea78138f5496f441bec942faae5
Robert_Long <robert1225@vt.edu>
2024-02-26 18:39:17 -0500

    edited main.cpp so we can test the TCS34725 color sensor also added the TCS34725 library

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "arduino_freertos.h"
 #include <ros.h>
 #include <geometry_msgs/Twist.h>
+#include <Adafruit_TCS34725.h>
 
 #include <spin_task.h>
 #include <Chassis.h>
@@ -38,6 +39,30 @@ void setup() {
   //   while(1);
   // }
 */
+  Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
+  if (tcs.begin() == false) {
+    while(1) {
+      Serial.println("Unable to find color sensor!!");
+    }
+  }
+  
+  uint16_t clear, red, green, blue;
+  while(1) {
+    tcs.setInterrupt(false);
+    delay(60);
+    
+    tcs.getRawData(&clear, &red, &green, &blue);
+    Serial.print("\nclear: ");
+    Serial.println(clear);
+    Serial.print("red: ");
+    Serial.println(red);
+    Serial.print("green: ");
+    Serial.println(green);
+    Serial.print("blue: ");
+    Serial.println(blue);
+
+    delay(1000);
+  }
 
   QTR_8 qtr(4);
   qtr.initTask();
