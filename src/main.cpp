e8fdc796d683fa1c5c7698cdaf61906586d60387
jdv52 <jaysondv0928@gmail.com>
2024-03-04 14:58:10 -0500

    Added listener on servo commands to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,7 @@
 #include "arduino_freertos.h"
 #include <ros.h>
 #include <geometry_msgs/Twist.h>
-
-#include <spin_task.h>
+#include <std_msgs/Int16.h>
 
 #include "Chassis.hpp"
 #include "MotorControl.h"
@@ -19,30 +18,43 @@ MotorControl m2(MOTOR_FR_IN1, MOTOR_FR_IN2);
 MotorControl m3(MOTOR_BR_IN1, MOTOR_BR_IN2);
 MotorControl m4(MOTOR_BL_IN1, MOTOR_BL_IN2);
 
+Servo thruster_arm_servo;
+Servo thruster_rel_servo;
+
 void sub_cb(const geometry_msgs::Twist& msg)
 {
   chassis.meccanum_kinematics(msg);
 }
 
-ros::Subscriber<geometry_msgs::Twist> sub("/cmd_vel", &sub_cb);
+void thruster_arm_pub_cb(const std_msgs::Int16& msg)
+{
+  thruster_arm_servo.write(msg.data);
+}
 
-Servo servo;
+void thrust_rel_pub_cb(const std_msgs::Int16& msg)
+{
+  thruster_rel_servo.write(msg.data);
+}
+
+
+ros::Subscriber<geometry_msgs::Twist> sub("/cmd_vel", &sub_cb);
+ros::Subscriber<std_msgs::Int16> thruster_arm_sub("/thruster_angle", &thruster_arm_pub_cb);
+ros::Subscriber<std_msgs::Int16> thruster_rel_sub("/thruster_release", &thrust_rel_pub_cb);
+
 
 FLASHMEM __attribute__((noinline)) void setup()
 {
-  // servo.attach(6);
-  // std::vector<MotorControl> motors;
-  // motors.push_back(m1); // FL
-  // motors.push_back(); // FR
-  // motors.push_back(); // BR
-  // motors.push_back(); // BL
-
+  thruster_arm_servo.attach(2);
+  thruster_rel_servo.attach(11);
   pinMode(arduino::LED_BUILTIN, arduino::OUTPUT);
   digitalWriteFast(arduino::LED_BUILTIN, arduino::HIGH);
 
   nh.getHardware()->setBaud(115200);
   nh.initNode();
+
   nh.subscribe(sub);
+  nh.subscribe(thruster_arm_sub);
+  nh.subscribe(thruster_rel_sub);
 }
 
 void loop()
@@ -52,10 +64,8 @@ void loop()
   char buff[50];
   sprintf(buff, "%d %d %d %d", (int)speds[0], (int)speds[1], (int)speds[2], (int)speds[3]);
   nh.loginfo(buff);
-  // sprintf(buff, "%d %d %d %d", m1.getSpeed(), m2.getSpeed(), m3.getSpeed(), m4.getSpeed());
 
   m1.Motor_start((int)speds[0]);
-  // m1.logState(nh);
   m2.Motor_start((int)speds[1]);
   m3.Motor_start((int)speds[2]);
   m4.Motor_start((int)speds[3]);
@@ -63,13 +73,4 @@ void loop()
   nh.spinOnce();
   
   delay(100);
-
-  // servo.write(110); // 75 for full forward, 120 for reverse
-  // delay(3000);
-  // servo.write(0);
-  // delay(1000);
-  // servo.write(75);
-  // delay(3000);
-  // servo.write(0);
-  // delay(5000);
 }
